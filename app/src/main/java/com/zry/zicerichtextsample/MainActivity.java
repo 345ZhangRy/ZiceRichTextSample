d3f562310bd4cdcddfa1c6eccb74db69d786cb10
ruoyu.zhang <ruoyu.zhang@net263.com>
2021-08-06 16:27:11 +0800

    delete useless import.

--- app/src/main/java/com/zry/zicerichtextsample/MainActivity.java
+++ app/src/main/java/com/zry/zicerichtextsample/MainActivity.java
@@ -1,7 +1,6 @@
 package com.zry.zicerichtextsample;
 
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
@@ -10,10 +9,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContract;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,7 +18,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.zhihu.matisse.Matisse;
 import com.zhihu.matisse.MimeType;
